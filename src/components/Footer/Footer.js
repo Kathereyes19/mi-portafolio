ac3f8834942df07b423157f2bc00a6b47cef2cc3
Kathereyes19 <kathe2004119@gmail.com>
2025-08-20 19:42:08 -0500

    Organización footer y flavicon

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Footer.css';
+import { useTheme } from '../../context/ThemeContext';
+import { useTranslation } from 'react-i18next';
 
 const Footer = () => {
+  const { theme } = useTheme(); // Obtenemos el tema actual
+  const { t } = useTranslation(); // Función de traducción
+
   return (
-    <footer className="site-footer">
-      <p>© {new Date().getFullYear()} Katherine Reyes - Diseñadora de medios interactivos</p>
+    <footer className={`site-footer ${theme === 'dark' ? 'dark' : 'light'}`}>
+      <p>© {new Date().getFullYear()} {t('footer.name')} - {t('footer.role')}</p>
     </footer>
   );
 };
